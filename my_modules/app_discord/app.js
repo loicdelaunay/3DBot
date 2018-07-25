ba2c29e4f9476e7aa39ebb6dbb5123aa57349a94
loïc delaunay <loicdelaunay05@gmail.com>
2018-07-25 22:22:25 +0200

    Init : struct 3

--- my_modules/app_discord/app.js
+++ my_modules/app_discord/app.js
@@ -1,19 +1,44 @@
 const bot = new global.module.discord.Client();
 
 bot.on('ready', function () {
-    global.mymodule.logmanager.addLog('Bot Discord Ready',global.enum.type.info,global.enum.context.discordjs);
-})
-
+    global.mymodule.logmanager.addLog('Bot Discord Ready', global.enum.type.info, global.enum.context.discordjs);
+});
 bot.login(global.setting.discordToken);
 
+
+//Router message
 bot.on('message', message => {
     if (message.content === 'ping') {
-        message.reply('pong !')
+        message.reply('pong !');
+        discordEmbed('pong', 0x42c2f4, "test", fields, message.channel);
     }
-})
+});
 
-bot.on('message', message => {
-    if (message.content === 'quoi') {
-        message.reply('feur !')
-    }
-})
+/**
+ * Send embed discord message
+ * @param title title of the embed
+ * @param color color of teh embed
+ * @param description description of the embed
+ * @param fields fileds off the embed field = {name,value}
+ * @param channel channel where message send
+ */
+function discordEmbed(title, color, description, fields, channel) {
+    let embed = new global.module.discord.RichEmbed()
+        .setTitle("   ❓   " + title + ":   ❓   ")
+        .setColor(color)
+        .setDescription(description)
+        .setFooter('© 3DBOT copyright Dream')
+        .setTimestamp()
+
+        .addField('\u200B', '\u200B');
+
+    fields.forEach(function (field) {
+        embed.addField(field.name, field.value)
+    });
+
+    embed.addField('\u200B', '\u200B');
+
+    channel.send({
+        embed
+    });
+}
